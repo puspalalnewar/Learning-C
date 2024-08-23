59aa2f9bb6415e1618bced3763c5149d5d99b025
Puspalal <puspalalnewar79@gmail.com>
2024-08-23 07:53:47 +0530

    Added files

--- Linkedlist/basic.c
+++ Linkedlist/basic.c
@@ -6,6 +6,24 @@ struct Node {
     struct Node* next;
 };
 
+void printLinkedList(struct Node* ptr){
+    struct Node* temp = ptr;
+    while (temp != NULL)
+    {
+        printf("Element : %d\n", temp->data);
+        temp = temp->next;
+    }
+    
+}
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
 
 int main(){
     struct Node* head;
@@ -22,6 +40,8 @@ int main(){
     second -> next = third;
     third->data = 45;
     third -> next = NULL;
+
+    printLinkedList(second);
     
     return 0;
 }
