39440f7708831a8578798b8955975faa60941a4c
Puspalal <puspalalnewar79@gmail.com>
2024-09-02 15:01:51 +0530

    Learned some basic of linkedlist

--- Linkedlist/basic.c
+++ Linkedlist/basic.c
@@ -6,45 +6,60 @@ struct Node {
     struct Node* next;
 };
 
-
 // Function to create a new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    // printf("Enter Data: ");
+    // scanf("%d", &newNode->data);
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
 
-void printLinkedList(struct Node* ptr){
-    struct Node* temp = ptr;
+void insertAtBeg(struct Node** head, int data){
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void printLinkedList(struct Node* head){
+    struct Node* temp = head;
     while (temp != NULL)
     {
-        printf("Element : %d\n", temp->data);
+        printf("%d -> ", temp->data);
         temp = temp->next;
     }
+    printf("NULL");
     
 }
 
-
-
-
 int main(){
-    struct Node* head;
-    struct Node* second;
-    struct Node* third;
-
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
-
-    head->data = 7;
-    head->next = second;
-    second->data = 12;
-    second -> next = third;
-    third->data = 45;
-    third -> next = NULL;
-
-    printLinkedList(second);
+    // int i = 0; 
+    // while(i != '\n'){
+    //     createNode()
+    // }
     
+    // for(int i = 0; i<10; i++){
+    //     printf("Enter Your Data : ");
+    //     int data = scanf("%d", &data);
+    //     // struct Node* head = createNode(data);
+    //     insertAtBeg(&head, data);
+    //     printf("If You Want To Add Press 1.If You Don't Press 0 ");
+    //     int response = scanf("%d", &response);
+    //     if(response == 0){
+    //         break;
+    //     }
+    // }
+    // struct Node* head = createNode();
+    // insertAtBeg(&head, 5);
+    // insertAtBeg(&head, 2);
+    // insertAtBeg(&head, 5);
+    struct Node* head = createNode(5);
+    struct Node* second = createNode(4);
+
+    head->next = second;
+    second ->next = NULL;
+    
+    printLinkedList(head);
     return 0;
 }
